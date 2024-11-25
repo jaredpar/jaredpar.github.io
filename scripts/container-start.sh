218552b9e8275be0ef497b6df0146daaec4e25d8
Jared Parsons <jared@paranoidcoding.org>
2024-11-25 21:15:22 +0000

    Fixed startup script

--- scripts/container-start.sh
+++ scripts/container-start.sh
@@ -1,4 +1,5 @@
 set -e
 
 /usr/local/bundle/bin/bundle install
-/usr/local/bundle/bin/bundle exec jekyll serve --watch
+/usr/local/bundle/bin/bundle exec jekyll serve --watch --port 4000 &
+echo Jekyll running on port 4000
